10ddc60848e784925484a270e1030d1f13b85578
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-04-12 16:14:13 +0530

    db.php + fix signups

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+$server_name = "localhost";
+$username = "root";
+$password = "";
+$database_name = "ElevateX";
+
+$conn = mysqli_connect($server_name, $username, $password, $database_name);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql ="CREATE DATABASE IF NOT EXISTS $database_name";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
